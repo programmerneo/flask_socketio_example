3084194d87e46d2e6c5412ea97d168877e030f4f
programmerneo <programmerneo@gmail.com>
2016-06-14 13:44:29 -0500

    adding a timer

--- hello.py
+++ hello.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
+import time
+from threading import Thread
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
@@ -10,21 +13,31 @@ socketio = SocketIO(app)
 
 @app.route('/')
 def index():
+    t = Thread(target=background_stuff)
+    t.start()
     return render_template('index.html')
 
 @socketio.on('value changed')
 def value_changede(message):
     print(message['data'])
-    socketio.emit('update value', message, broadcast=True)
+    socketio.emit('update value', message)
 
 @socketio.on('connect')
 def test_connect():
     print 'Connected'
     socketio.send('Connected')
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on('disconnect')
 def test_disconnect():
     print('Client disconnected')
+
+def background_stuff():
+     print 'In background_stuff'
+     while True:
+         time.sleep(1)
+         t = time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
+         print t
+         socketio.emit('the time', {'time': t})
 
 if __name__ == '__main__':
     socketio.run(app)
